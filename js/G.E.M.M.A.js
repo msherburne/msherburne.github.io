c8c0dd68493df5308944fd20e4bcf4e25066c751
TheIDEKS <mathieusherburne@outlook.com>
2021-02-16 21:52:04 -0800

    Updated

--- js/G.E.M.M.A.js
+++ js/G.E.M.M.A.js
@@ -13,3 +13,27 @@ function cuteness() {
     p.innerHTML = "Your smile is something worth waiting all day for. When I'm on my way to you there's a smile on my face and excitment in my heart. When I get to stare at your face it's really just mesmerizing. You seem to be comfortable in your own skin; as you should be, because you are beautiful."
     tit.innerHTML ="You're Adorable!"
 }
+
+function dogs() {
+    let p = document.getElementById("conP");
+    let tit = document.getElementById("conTitle");
+
+    p.innerHTML = "Your dogs are so wholesome and sweet, except for one tiny devil. I think its amazing that you have your life together so that you can have things most 22 year olds don't. Like a house. or Dogs. Anyways... I really like your dogs."
+    tit.innerHTML ="Cutest Babies"
+}
+
+function intel() {
+    let p = document.getElementById("conP");
+    let tit = document.getElementById("conTitle");
+
+    p.innerHTML = "Don't even deny it. You are litterely have an entire encyclopedia in your head. Your critical thinking skills are off the charts and you have deep understanding for a lot of things I could never even comprehend. I appreciate how smart you are. You're like my own personal library."
+    tit.innerHTML ="You're So Much Smarter Than Me!"
+}
+
+function humor() {
+    let p = document.getElementById("conP");
+    let tit = document.getElementById("conTitle");
+
+    p.innerHTML = "Which is saying a lot, because most people I just fake laugh for. Sometimes you just say things that catch me off guard as well. Those are especially funny. Sometimes when you talk about work I chuckle because of something you told me earlier that was really funny was given context by your co-workers. I really apreciate that about you."
+    tit.innerHTML = "You Acually Make Me Laugh"
+}
