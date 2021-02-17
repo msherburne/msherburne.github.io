1dfe9c21e6fda1075fc8a8e15d6eda8901429a25
TheIDEKS <mathieusherburne@outlook.com>
2021-02-16 21:29:06 -0800

    Updates.Gemma.Plat

---
+++ js/G.E.M.M.A.js
@@ -0,0 +1,15 @@
+function personality() {
+    let p = document.getElementById("conP");
+    let tit = document.getElementById("conTitle");
+
+    p.innerHTML ="You have such a great personality. I don't know if its charisma or wit, but I know that when I talk to you all I want to do is listen and see life through your eyes. Although you are highly non-argumentative, I believe in time you will never hessitate to tell me anything. I appreciate how amazing you are!"
+    tit.innerHTML ="Your Personality"
+}
+
+function cuteness() {
+    let p = document.getElementById("conP");
+    let tit = document.getElementById("conTitle");
+
+    p.innerHTML = "Your smile is something worth waiting all day for. When I'm on my way to you there's a smile on my face and excitment in my heart. When I get to stare at your face it's really just mesmerizing. You seem to be comfortable in your own skin; as you should be, because you are beautiful."
+    tit.innerHTML ="You're Adorable!"
+}
---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+
+function showCoolStuff() {
+    let text = document.getElementById("cute");
+    text.style.visibility = "visible";
+    let but = document.getElementById("epic");
+    but.style.visibility = "visible";
+}
