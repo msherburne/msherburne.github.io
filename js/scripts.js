9843b75fb65dc9830dce964578dcdeacd13ed34a
Mathieu Sherburne <mathieusherburne@outlook.com>
2021-03-13 09:47:56 -0800

    change

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-
-function showCoolStuff() {
-    let text = document.getElementById("cute");
-    text.style.visibility = "visible";
-    let but = document.getElementById("epic");
-    but.style.visibility = "visible";
-    let cool = document.getElementById("cool");
-    cool.style.visibility = "visible";
-}
