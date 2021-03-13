7b8052e3e22ee77793e962c4a65295b855549ba8
Mathieu Sherburne <mathieusherburne@outlook.com>
2021-03-13 10:52:36 -0800

    c

--- js/index.js
+++ js/index.js
@@ -1,38 +1,7 @@
-var provider = new firebase.auth.GoogleAuthProvider();
-
-firebase.auth()
-  .getRedirectResult()
-  .then((result) => {
-    if (result.credential) {
-      /** @type {firebase.auth.OAuthCredential} */
-      var credential = result.credential;
-
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = credential.accessToken;
-      // ...
-    }
-    // The signed-in user info.
-    var user = result.user;
-  }).catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-  
-function select() {
-    let sel = document.getElementById('login_div')
-    let sign = document.getElementById('selection')
-
-    sel.style.visibility = 'hidden';
-    sign.style.visibility = 'visible';
-}
-
-
-function login() {
-    firebase.auth().signInWithRedirect(provider);
-}
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
